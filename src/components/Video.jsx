48c4c6ba1a7162f07c845a95baaa159e4ceece82
Molar <grubenmj@gmail.com>
2024-06-12 20:54:42 -0600

    some feactures added

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,8 +1,27 @@
+import clsx from "clsx"
+
 export default function Video({image,title,channel}){
-    return(<article className="video-card">
+    // const articleClassnames =
+    // "video-card" + (channel === "Marques Brownlee" ? " special" : " ")
+
+    return(<article className={clsx('video-card', {'special': !channel})}>
         <img src={image} alt="" />
         <footer>
-            <img src={`https://api.dicebear.com/8.x/notionists-neutral/svg?seed=${channel}`} alt="" />
+            {/* {
+                channel && (
+                    <img src={`https://api.dicebear.com/8.x/notionists-neutral/svg?seed=${channel}`} alt="" />
+                )
+            }
+            {
+                !channel && <span>🤷‍♂️</span>
+            } */}
+            {
+                channel ? (
+                    <img src={`https://api.dicebear.com/8.x/notionists-neutral/svg?seed=${channel}`} alt="" />
+                ):(
+                    <span>🤷‍♂️</span>
+                )
+            }
             <p className="video-card-title">{title}</p>
             <p className="video-card-channel">{channel}</p>
         </footer>
